24dd68d0d7e7366bb8a002645385a96428de5b42
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2025-11-20 23:06:37 +0100

    ajouter le js du info d'employe

--- main.js
+++ main.js
@@ -2,6 +2,33 @@ const ajouter  = document.getElementById('Ajouter');
 const container2 = document.getElementById('container2');
 const close = document.getElementById('close');
 const closex = document.getElementById('closex');
+const closex2 = document.getElementById('closex2');
+let enrgEmp = document.getElementById('enregistrer')
+const openInfo  = document.getElementById('btn_info');
+const container_3 = document.getElementById('container_3');
+
+
+openInfo.addEventListener('click', () =>{
+
+container_3.classList.add('show')
+
+
+
+}) ;
+
+closex2.addEventListener('click', () =>{
+
+container_3.classList.remove('show')
+
+
+
+}) ;
+
+
+
+
+
+
 
 
 ajouter.addEventListener('click', () =>{
@@ -26,5 +53,23 @@ closex.addEventListener('click', () =>{
 
 
 }) ;
+
+
+// create employe
+
+
+let dataEmploye = [];
+
+enrgEmp.onclick = function (){
+
+
+   let newEmp = {
+
+
+     
+   }
+
+
+}
 
 
